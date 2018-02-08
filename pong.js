1523904556e3cb0345d4245265e586b0e0c68a29
thisAtlas <magnus.kartvedt.p@gmail.com>
2018-02-08 10:25:12 +0100

    Created gameloop function, draw paddles function
    Added many things: instructions to get canvas information, created
    context object, settings function (empty so far), gameloop function
    (clears canvas and draws paddles), drawPaddle function

--- pong.js
+++ pong.js
@@ -1,6 +1,31 @@
-
+var canvas;
+var ctx;
 
 function onload() {
     "use strict";
+    canvas = document.getElementById("canvas"); //Saves ref. to id:canvas in var
+    ctx = canvas.getContext('2d'); //Creates a "CanvasRenderingContext2D"-object
     
+    settings();
+    gameloop();
+}
+
+function settings() {
+    
+}
+
+function gameloop() {
+    //clears canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    drawPaddle(xPadding, playerYPos, PWidth, PHeight);
+    drawPaddle(canvas.width - xPadding - PWidth, AIPy, PHeight);
+    
+}
+
+function drawPaddle(cornerx, cornery, width, height) {
+    ctx.beginPath();
+    ctx.rect(cornerx, cornery, width, height);
+    ctx.fillStyle = "#eeeeee";
+    ctx.fill();
 }
