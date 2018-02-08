3cfd8f6cb15bd7da44c1ddd38ad7fff56c20946f
thisAtlas <magnus.kartvedt.p@gmail.com>
2018-02-08 10:51:46 +0100

    Created 2 paddles on the canvas (POC: Works)
    Declared settings for paddle position, dimenions and created a function
    to draw them.
    Includes 'requestAnimationFrame(draw);' which repeats the function draw.

--- pong.js
+++ pong.js
@@ -1,17 +1,33 @@
+
+//List of global variables used throughout the code. Most are simply declared, then later adjusted in settings().
 var canvas;
 var ctx;
+//paddle variables
+var PWidth;     //Paddle width.
+var PHeight;    //Paddle height.
+
+var xPadding;   //padding (distance from) on the x-axis.
+
+var playerYPos;
+var AIYPos;
 
 function onload() {
     "use strict";
-    canvas = document.getElementById("canvas"); //Saves ref. to id:canvas in var
-    ctx = canvas.getContext('2d'); //Creates a "CanvasRenderingContext2D"-object
+    canvas = document.getElementById("canvas");    //Saves ref. to id:canvas in var
+    ctx = canvas.getContext('2d');    //Creates a "CanvasRenderingContext2D"-object
     
     settings();
     gameloop();
 }
 
 function settings() {
+    PWidth = canvas.width/250; 
+    PHeight = canvas.height/10;
     
+    xPadding = canvas.width/40
+    
+    playerYPos = (canvas.height / 2) - (PHeight / 2);
+    AIYPos = (canvas.height / 2) - (PHeight / 2);
 }
 
 function gameloop() {
@@ -19,8 +35,9 @@ function gameloop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     drawPaddle(xPadding, playerYPos, PWidth, PHeight);
-    drawPaddle(canvas.width - xPadding - PWidth, AIPy, PHeight);
+    drawPaddle(canvas.width - xPadding - PWidth, AIYPos, PWidth, PHeight);
     
+    requestAnimationFrame(draw);
 }
 
 function drawPaddle(cornerx, cornery, width, height) {
